f6c00ecba864e88e99252c923a322f7b5f4ec822
TemuulenBM <temuulen.developer@gmail.com>
2024-04-03 21:03:24 +0800

    fixed hydration error

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,7 +2,6 @@ import { db } from "@/lib/db";
 import { getSelf } from "@/lib/auth-service";
 
 export const getRecommended = async () => {
-
     const users = await db.user.findMany({
         orderBy: {
             createdAt: "desc"
