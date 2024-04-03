99148bf755196b16412b2c09ccb2d8739c0fdd02
Temuulen <124872945+TemuulenBM@users.noreply.github.com>
2024-04-03 21:06:08 +0800

    Revert "dev main branch"

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // for more information about configuring your Middleware
 export default authMiddleware({
     // Allow signed out users to access the specified routes:
-    publicRoutes: [
-        "/",
-        "/api/webhooks(.*)"
-    ],
+    // publicRoutes: ['/anyone-can-visit-this-route'],
 });
 
 export const config = {
